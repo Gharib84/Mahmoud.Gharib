614e9ffd734ae462b67db4b3990b0f1886965764
Gharib84e <ITprofessional63@gmail.com>
2025-04-23 19:11:42 +0200

    fixed fonts type

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
+import { IntroductionComponent } from './components/introduction/introduction.component';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent,IntroductionComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
