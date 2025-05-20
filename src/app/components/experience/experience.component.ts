2ec0dc3c225a2de50c2bb86be3d2e67a5feb199d
Gharib84e <ITprofessional63@gmail.com>
2025-05-20 19:49:10 +0200

    english json file updated & modified

--- src/app/components/experience/experience.component.ts
+++ src/app/components/experience/experience.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
-
+import { TranslatePipe } from '@ngx-translate/core';
+import { PageSection } from '../../core/page-section';
 @Component({
   selector: 'app-experience',
   imports: [],
   templateUrl: './experience.component.html',
   styleUrl: './experience.component.css'
 })
-export class ExperienceComponent {
+  export class ExperienceComponent {
 
+  pageSection: PageSection = {
+    title: 'Experience'
+  }
 }
