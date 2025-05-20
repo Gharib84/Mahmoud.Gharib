811edb19049788a8a75fce942d652124b3b1232b
Gharib84e <ITprofessional63@gmail.com>
2025-05-20 20:51:00 +0200

    accordion style had been finished for dark mode only

--- src/app/components/experience/experience.component.ts
+++ src/app/components/experience/experience.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { TranslatePipe } from '@ngx-translate/core';
 import { PageSection } from '../../core/page-section';
+import { AccordionComponent } from '../../shared/accordion/accordion.component';
 @Component({
   selector: 'app-experience',
-  imports: [],
+  imports: [TranslatePipe, AccordionComponent],
   templateUrl: './experience.component.html',
   styleUrl: './experience.component.css'
 })
