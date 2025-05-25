fbfc3338d3270789eb4e63024fa183df2782974e
Gharib84e <ITprofessional63@gmail.com>
2025-05-25 15:29:02 +0200

    code refactory css

--- src/app/core/service/experience.service.ts
+++ src/app/core/service/experience.service.ts
@@ -13,8 +13,8 @@ export class ExperienceService {
       platform: 'Platform X',
       type: 'Full-time',
       title: 'Full Stack Developer',
-      start: new Date('2022-01-01'),
-      end: new Date('2022-12-31'),
+      start: '03-2023',
+      end: '11-2024',
       tasks: [
         'Developed web applications using Angular and Node.js',
         'Implemented RESTful APIs using Express.js',
@@ -29,8 +29,7 @@ export class ExperienceService {
       platform: 'Platform Y',
       type: 'Part-time',
       title: 'Front-end Developer',
-      start: new Date('2021-06-01'),
-      end: new Date('2021-09-30'),
+      start: 'present',
       tasks: [
         'Designed and implemented responsive user interfaces using HTML, CSS, and JavaScript',
         'Collaborated with cross-functional teams to deliver high-quality software',
--- src/app/shared/accordion/accordion.component.ts
+++ src/app/shared/accordion/accordion.component.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { Component,inject,OnInit,OnDestroy } from '@angular/core';
 import { AccordionTriggerDirective } from '../../core/directive/accordion-trigger.directive';
+import { Experience } from '../../core/experience';
+import { ExperienceService } from '../../core/service/experience.service';
+import { Subscription } from 'rxjs';
 @Component({
   selector: 'app-accordion',
   imports: [AccordionTriggerDirective],
   templateUrl: './accordion.component.html',
   styleUrl: './accordion.component.css'
 })
-export class AccordionComponent {
-
+export class AccordionComponent implements OnInit, OnDestroy {
+   experiences: Experience[] = [];
+   private subsscription!:Subscription 
+   private experienceService = inject(ExperienceService)
+  ngOnInit(): void {
+    this.subsscription= this.experienceService.getExperiences().subscribe({
+      next: (experiences) => {
+        this.experiences = experiences;
+      },
+      error: (err) => {
+        console.log(err);
+      }
+    })
+  }
+  ngOnDestroy(): void {
+    if(this.subsscription){
+      this.subsscription.unsubscribe()
+    }
+  }
 }
