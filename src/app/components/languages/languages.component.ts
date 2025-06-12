522e59b4b3db723e9c20d2e83e8383fbee042aa0
Gharib84e <ITprofessional63@gmail.com>
2025-06-12 20:06:21 +0200

    languages menu UI

--- src/app/components/languages/languages.component.ts
+++ src/app/components/languages/languages.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { Language } from '../../core/language';
 @Component({
   selector: 'app-languages',
   imports: [],
@@ -8,17 +8,17 @@ import { Component } from '@angular/core';
 <div class="relative w-36 text-gray-200">
   <!-- Selected Item Button -->
   <!-- The blue border indicates this element is active/open -->
-  <button
+  <button (click)="toggleDropdown()"
     class="flex items-center justify-between w-full p-3  dark:bg-background-dark rounded-lg"
   >
     <div class="flex items-center">
       <!-- Uk Flag -->
       <img
-        src="https://flagcdn.com/w40/gb.png"
-        alt="United Kingdom flag"
+        [src]="selectedLanguage.flagUrl"
+        [alt]="selectedLanguage.name + ' flag'"
         class="w-5 h-5 rounded-full mr-3"
       />
-      <span class="text-accent dark:text-foreground hover:text-blue-500 hover:font-bold font-inter not:italic">UK</span>
+      <span class="text-accent dark:text-foreground hover:text-blue-500 hover:font-bold font-inter not:italic">{{selectedLanguage.code.toUpperCase()}}</span>
     </div>
     <!-- Chevron Up Icon (indicating the dropdown is open) -->
     <svg
@@ -36,35 +36,65 @@ import { Component } from '@angular/core';
     </svg>
   </button>
   <!-- Dropdown Menu -->
+  @if (isOpen) {
   <div
     class="absolute top-full mt-1 w-full dark:bg-background-dark rounded-lg shadow-lg z-10"
   >
-    <ul class="text-dark dark:text-primary hidden">
+    <ul class="text-dark dark:text-primary">
       <!-- Dropdown Item -->
-      <li class="flex items-center p-3 cursor-pointer hover:bg-background-dark hover:dark:bg-gray-700/50">
+      @for (lang of languages ; track lang) { 
+        
+      <li class="flex items-center p-3 cursor-pointer hover:bg-background-dark hover:dark:bg-gray-700/50
+        " (click)="selectLanguage(lang)">
         <img
-          src="https://flagcdn.com/w40/pl.png"
-          alt="Poland flag"
+          [src]="lang.flagUrl"
+          [alt]="lang.name + ' flag'"
           class="w-5 h-5 rounded-full mr-3"
         />
-        <span class="font-medium text-accent dark:text-foreground hover:text-blue-500 hover:font-bold">PL</span>
+        <span class="font-medium text-accent dark:text-foreground hover:text-blue-500 hover:font-bold">
+          {{ lang.code.toUpperCase() }}
+        </span>
       </li>
-      <!-- Dropdown Item -->
-      <li class="flex items-center p-3 cursor-pointer hover:bg-background-dark hover:dark:bg-gray-700/50">
-        <img
-          src="https://flagcdn.com/w40/de.png"
-          alt="Germany flag"
-          class="w-6 h-6 rounded-full mr-3"
-        />
-        <span class="font-medium text-accent dark:text-foreground hover:text-blue-500 hover:font-bold">DE</span>
-      </li>     
+      }
     </ul>
   </div>
+  }
 </div>
 
   `,
   styles: ``
 })
 export class LanguagesComponent {
+  isOpen: boolean = false;
+  selectedLanguage = {
+    code: 'gb',
+    name: 'United Kingdom',
+    flagUrl: 'https://flagcdn.com/w40/gb.png'
+  };
 
+  languages: Language[] = [
+    { code: 'gb', name: 'United Kingdom', flagUrl: 'https://flagcdn.com/w40/gb.png' },
+    { code: 'pl', name: 'Poland', flagUrl: 'https://flagcdn.com/w40/pl.png' },
+    { code: 'de', name: 'Germany', flagUrl: 'https://flagcdn.com/w40/de.png' },
+  ];
+
+
+  /**
+   * Toggles the dropdown menu's open/closed state.
+   * If the dropdown is currently open, it will be closed and vice versa.
+   */
+
+  toggleDropdown() {
+    this.isOpen = !this.isOpen;
+  }
+
+  /**
+   * Selects a language from the dropdown menu.
+   * This method updates the selected language and closes the dropdown.
+   * @param language - The language object to select.
+   */
+  selectLanguage(language: Language): void {
+    this.selectedLanguage = language;
+    this.isOpen = false;
+  }
 }
