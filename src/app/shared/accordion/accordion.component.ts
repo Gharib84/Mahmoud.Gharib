c7ebd4c1f03b7e3d6e922ce350b41184965334aa
Gharib84e <ITprofessional63@gmail.com>
2025-06-15 18:00:00 +0200

    polish experience section had been done

--- src/app/shared/accordion/accordion.component.ts
+++ src/app/shared/accordion/accordion.component.ts
@@ -3,8 +3,7 @@ import { AccordionTriggerDirective } from '../../core/directive/accordion-trigge
 import { Experience } from '../../core/experience';
 import { ExperienceService } from '../../core/service/experience.service';
 import { Subscription } from 'rxjs';
-import {TranslatePipe} from "@ngx-translate/core";
-
+import {TranslatePipe,TranslateService} from "@ngx-translate/core";
 @Component({
   selector: 'app-accordion',
   imports: [TranslatePipe, AccordionTriggerDirective],
@@ -14,7 +13,9 @@ import {TranslatePipe} from "@ngx-translate/core";
 export class AccordionComponent implements OnInit, OnDestroy {
    experiences: Experience[] = [];
    private subsscription!:Subscription 
-   private experienceService = inject(ExperienceService)
+   private experienceService = inject(ExperienceService);
+   polishExperiences: Experience[] = [];
+   private translateService = inject(TranslateService);
   ngOnInit(): void {
     this.subsscription= this.experienceService.getExperiences().subscribe({
       next: (experiences) => {
@@ -23,11 +24,25 @@ export class AccordionComponent implements OnInit, OnDestroy {
       error: (err) => {
         console.log(err);
       }
-    })
+    });
+
+    this.subsscription= this.experienceService.getPolishExperiences().subscribe({
+      next: (polishExperiences) => {
+        this.polishExperiences = polishExperiences;
+      },
+      error: (err) => {
+        console.log(err);
+      }
+    });
   }
   ngOnDestroy(): void {
     if(this.subsscription){
       this.subsscription.unsubscribe()
     }
   }
+
+  get ExperienceObje():Experience[] {
+    return this.translateService.currentLang === 'pl' ? this.polishExperiences : this.experiences;
+  }
+  
 }
