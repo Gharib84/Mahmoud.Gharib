aa678150a19b73ad740412ad3d1a052097118203
Gharib84e <ITprofessional63@gmail.com>
2025-10-12 20:49:53 +0200

    style(projects card): update card style

--- src/app/core/service/projects-detail.service.ts
+++ src/app/core/service/projects-detail.service.ts
@@ -7,30 +7,44 @@ export class ProjectsDetailService {
   private projectsDetails = signal<ProjectDetail[]>([
     {
       id: 1,
-      title: 'Portfolio',
-      description: 'My personal portfolio website showcasing my skills and projects.',
-      image: 'assets/images/portfolio.png',
-      link: 'https://my-portfolio.com',
-      techStack: ['Angular', 'TypeScript', 'CSS'],
-      githubLink: 'https://github.com/yourusername/my-portfolio'
+      title: 'E-commerce API',
+      description: 'A full-featured e-commerce Api.',
+      image: 'assets/images/ecommerce.png',
+      link: {
+        github: 'https://github.com/yourusername/my-ecommerce-app'
+      },
+      techStack: ['Nest', 'TypeORM', 'PostgreSQL', 'Swagger', 'JWT'],
+      githubLink: 'https://github.com/Gharib84/E-Commerce-API'
     },
     {
       id: 2,
-      title: 'E-commerce App',
-      description: 'A full-featured e-commerce application with user authentication and payment integration.',
-      image: 'assets/images/ecommerce.png',
-      link: 'https://my-ecommerce-app.com',
-      techStack: ['React', 'Node.js', 'MongoDB'],
-      githubLink: 'https://github.com/yourusername/my-ecommerce-app'
-    },
-    {
-      id: 3,
-      title: 'Blog Platform',
-      description: 'A blogging platform where users can create and share articles.',
+      title: 'Dev News Blog',
+      description: 'A lightweight Angular application that integrates with the Dev.to API.',
       image: 'assets/images/blog.png',
-      link: 'https://my-blog-platform.com',
-      techStack: ['Vue.js', 'Express', 'MySQL'],
-      githubLink: ' https://github.com/yourusername/my-blog-platform'
+      link: {
+        demo: 'https://dev-news-two.vercel.app/',
+        github: 'https://github.com/Gharib84/Dev-news'
+      },
+      techStack: ['Angular', 'Firebase', 'Dev.to API', 'Tailwind CSS'],
+    },
+      {
+      id: 3,
+      title: 'Portfolio',
+      description: 'My personal portfolio website.',
+      image: 'assets/images/portfolio.png',
+      link: {
+        demo: 'https://my-portfolio.com',
+        github: 'https://github.com/Gharib84/Portfolio-25'
+      },
+      techStack: ['Angular', 'TypeScript', 'Tailwind CSS'],
     },    
-  ]);  
+  ]); 
+  
+/**
+ * Returns an observable of ProjectDetail[] which contains the details of all the projects.
+ * @returns {Observable<ProjectDetail[]>} An observable of ProjectDetail[].
+ */
+  getProjectsDetails() {
+    return this.projectsDetails();
+  }
 }
